06465da95b1c143628b76ce3cc7dffb24821165e
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-07-31 22:29:48 +0900

    BaseballMain 컴포넌트 App에 적용

--- number_baseball/src/App.js
+++ number_baseball/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import BaseballMain from "./components/baseballMain";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BaseballMain />
+    </div>
+  );
 }
 
 export default App;

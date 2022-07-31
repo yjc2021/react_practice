02f7f8a313f08cfc37eb567db0a980b6c70e4aac
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-07-31 22:30:39 +0900

    BaseballHistory 컴포넌트 생성

---
+++ number_baseball/src/components/baseballHistory.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BaseballHistory = ({ history }) => {
+  return (
+    <div>
+      <h3>히스토리</h3>
+      {history.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </div>
+  );
+};
+
+export default BaseballHistory;

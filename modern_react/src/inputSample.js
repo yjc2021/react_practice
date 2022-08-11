974034294c9ec6f78e3ff45f1344b42999023a32
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-11 13:51:33 +0900

    moder_react/10. useRef로 특정 DOM 선택하기

--- modern_react/src/inputSample.js
+++ modern_react/src/inputSample.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
-
+import React, { useRef, useState } from "react";
 const InputSample = () => {
   const [inputs, setInputs] = useState({
     name: "",
     nickname: "",
   });
-  // input이 여러개일 때는 각 input에 대하여 개별적인 useState과 onChange를 만들어서 구현하는 것은 비효율적
-  // 더 나은 방법은 useState에서 하나의 객체형태의 상태를 관리하고
-  // 각 input에 name attribute을 설정하고 이벤트가 발생했을 때 해당 name만 참조하는 것이다
+  const nameInput = useRef();
+  // 1. useRef를 사용하여 Ref객체를 만들고
 
   const { name, nickname } = inputs; // 객체 비구조화 할당
 
@@ -19,19 +17,25 @@ const InputSample = () => {
     });
   };
 
-  // 리액트에서 state을 수정할 때 불변성을 지켜야 한다
-  // state 수정 시 기존 객체를 수정하는 것이 아닌 새로운 수정된 객체를 만들어야 함
-  // 불변성을 지켜주어야만 컴포넌트 상태가 없데이트 됐음을 감지하고 필요한 리렌더링이 진행
   const onReset = () => {
     setInputs({
       name: "",
       nickname: "",
     });
+    nameInput.current.focus();
+    // 3. Ref객체의 .current 값은 선택한 DOM을 가리킴
   };
 
   return (
     <div>
-      <input placeholder="이름" name="name" onChange={onChange} value={name} />
+      <input
+        placeholder="이름"
+        name="name"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+        // 2. 선택하고 싶은 DOM에 ref값으로 설정
+      />
       <input
         placeholder="닉네임"
         name="nickname"

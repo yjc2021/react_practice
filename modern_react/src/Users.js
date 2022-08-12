a2996883f3b7179432b9ffba43d2c62b1cffce64
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-12 15:30:28 +0900

    modern_react/4.2 useReducer로 요청 상태 관리하기

--- modern_react/src/Users.js
+++ modern_react/src/Users.js
@@ -1,28 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import axios from "axios";
 
-const Users = (props) => {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+const Users = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
   const fetchUsers = async () => {
+    dispatch({
+      type: "LOADING",
+    });
     try {
-      setError(null);
-      setUsers(null);
-      setLoading(true);
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/users"
       );
-      setUsers(response.data);
+      dispatch({ type: "SUCCESS", data: response.data });
     } catch (e) {
-      setError(e);
+      dispatch({ type: "ERROR", error: e });
     }
-    setLoading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const { loading, data: users, error } = state;
 
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
@@ -47,7 +74,7 @@ export default Users;
 // 1. 요청의 결과
 // 2. 로딩 상태
 // 3. 에러
-// useState로 요청 상태를 관리, useEffect로 컴포넌트 렌더링 시점에 요청을 시작
+// useReducer로 요청 상태를 관리, useEffect로 컴포넌트 렌더링 시점에 요청을 시작
 
 // ** useEffect에 첫번째 파라미터로 등록하는 함수에는 async를 사용할 수 없기 때문에
 // 함수 내부에서 async를 사용하는 새로운 함수를 선언해줘야 함

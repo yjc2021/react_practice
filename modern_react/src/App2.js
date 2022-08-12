798ceaff68712f3e72a55f0910d0b46b79487201
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-08-12 15:20:49 +0900

    modern_react/4.1 API 연동의 기본

---
+++ modern_react/src/App2.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Users from "./Users";
+
+const App2 = (props) => {
+  return <Users />;
+};
+
+export default App2;
---
+++ modern_react/src/Users.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Users = (props) => {
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const fetchUsers = async () => {
+    try {
+      setError(null);
+      setUsers(null);
+      setLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(response.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return null;
+  return (
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}>다시 불러오기</button>
+    </>
+  );
+};
+
+export default Users;
+
+// API 요청에 대한 상태를 관리할 때에는 총 3가지 상태를 관리해야 한다
+// 1. 요청의 결과
+// 2. 로딩 상태
+// 3. 에러
+// useState로 요청 상태를 관리, useEffect로 컴포넌트 렌더링 시점에 요청을 시작
+
+// ** useEffect에 첫번째 파라미터로 등록하는 함수에는 async를 사용할 수 없기 때문에
+// 함수 내부에서 async를 사용하는 새로운 함수를 선언해줘야 함
